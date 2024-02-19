fb8ae5e693ee0923eb32ac92505d9587d629866a
NicolaTeora <nikxodia@gmail.com>
2024-02-19 16:49:02 +0100

    feat : add assets

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import NavBar from './components/NavBar.vue';
+export default {
+  data(){
+    return{
+      title:'ciao',
+    }
+  },
+  
+  components: { NavBar }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <nav-bar/>
 </template>
 
 <style lang="scss" >
---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data(){
+        return{
+            titlejumbo:'hello'
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container-two">
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/general.scss' as *;
+</style>
