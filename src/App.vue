155c4d23e3157a8b9484065859a92ba2e9b9adfb
NicolaTeora <nikxodia@gmail.com>
2024-02-20 09:43:11 +0100

    feat : dev section shop product

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
 import NavBar from './components/NavBar.vue';
 import Jumbo from './components/Jumbo.vue';
 import SectionOne from './components/SectionOne.vue';
+import ShopProductList from './components/ShopProductList.vue';
 
 export default {
   data(){
     return{}
   },
   
-  components: { NavBar, Jumbo, SectionOne }
+  components: { NavBar, Jumbo, SectionOne, ShopProductList }
 }
 </script>
 
@@ -16,6 +17,7 @@ export default {
   <nav-bar/> <!-- TODO: data props -->
   <jumbo/>
   <section-one/>
+  <shop-product-list/>
 </template>
 
 <style lang="scss">
---
+++ src/components/ShopProductList.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    data(){
+        return{}
+    }
+}
+</script>
+
+<template>
+    <div class="container-one">
+        <div class="container-two">
+            <p>Trending Online Store</p>
+            <h2>Gogrin all <span>organic</span> food</h2>
+        </div>
+
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../style/general.scss' as *;
+
+.container-one{
+    margin-top: 5rem;
+    background-image: url('../components/assets/img/shop-bg-img.jpg');
+    .container-two{
+        padding: 2rem;
+        text-align: center;
+    }
+}
+</style>
