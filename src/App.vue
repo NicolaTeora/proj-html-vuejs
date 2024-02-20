1257c4951a7a232386d4079ef4443d12db110b24
NicolaTeora <nikxodia@gmail.com>
2024-02-20 20:51:44 +0100

    feat : dev footer

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,46 @@ import NavBar from './components/NavBar.vue';
 import Jumbo from './components/Jumbo.vue';
 import SectionOne from './components/SectionOne.vue';
 import ShopProductList from './components/ShopProductList.vue';
-import NewsLetterApp from './components/NewsLetterApp.vue';
+//import NewsLetterApp from './components/NewsLetterApp.vue';
+import FooterApp from './components/FooterApp.vue';
 
 export default {
   data(){
-    return{}
+    return{
+      logoFooter: './assets/img/footer-logo.png',
+      socialsContact: [
+        {
+          tag:'fa-brands fa-facebook-f',
+          name: 'Facebook'
+        },
+        {
+          tag:'fa-brands fa-google-plus-g',
+          name: 'Google Plus'
+        },
+        {
+          tag:'fa-brands fa-x-twitter',
+          name: 'Twitter'
+        },
+        {
+          tag: 'fa-brands fa-pinterest-p fa-xs',
+          name: 'Pinterest'
+        }
+      ],
+
+      Blogs:[
+        {
+          imgBlog: './asset/img/blo1-70x70.jpg',
+          description: 'Where I live, I am surrounded by fresh, organic food, so I eat really well.',
+          data: 'December 17.2022',
+        }
+      ],
+      
+      titoloLinks: 'Emergency Link.',
+      links: ['Organic Food Shop', 'Our Term & Condition.', 'Gogrin Faq Section', 'Our Team Member', 'Our Latest News Feed']
+    }
   },
   
-  components: { NavBar, Jumbo, SectionOne, ShopProductList, NewsLetterApp }
+  components: { NavBar, Jumbo, SectionOne, ShopProductList, FooterApp }
 }
 </script>
 
@@ -20,7 +52,14 @@ export default {
   <section-one/>
   <shop-product-list/> <!-- TODO: fare funzioni calcolo sconto prodotti -->
 
-  <news-letter-app/>
+  <!-- <news-letter-app/> -->
+  <footer-app
+  :logoFooter="logoFooter"
+  :socials="socialsContact"
+  :name="name"
+  :tag="tag"
+  :titoloLinks="titoloLinks"
+  :links="links" />
 </template>
 
 <style lang="scss">
---
+++ src/components/FooterApp.vue
@@ -0,0 +1,80 @@
+<script>
+export default{
+    data(){
+        return{
+            istruction: 'War should never be entered upon every agency of peace has failed not to be good'
+        }
+    },
+
+    props:{
+        logoFooter: String,
+        socials: Array,
+        name: String,
+        tag: String,
+        links: Array,
+        titoloLinks: String,
+    },
+
+    methods:{
+        getImgPath(img){
+            return new URL(`${img}`, import.meta.url).href;
+        }
+    }
+}
+</script>
+
+<template>
+    <footer>
+        <div class="container-two">
+            <div class="logo-ft">
+                <ul>
+                    <li><img :src="getImgPath(logoFooter)" alt="logo footer"></li>
+                    <li>{{ istruction }}</li>
+                    <li>
+                        <ul class="socialLinks">
+                            <li v-for="social of socials"><i :class="social.tag"></i></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="emegency">
+                <h3>{{titoloLinks}}</h3>
+                <ul>
+                    <li v-for="link of links">{{ link }}</li>
+                </ul>
+            </div>
+            <div class="blogs">
+                
+            </div>
+    
+        </div>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/general.scss' as *;
+@use '../style/partials/mixins' as *;
+
+footer{
+    background-image: url('./assets/img/footer-bg.jpg');
+    color: whitesmoke;
+}
+.container-two{
+    display: flex;
+    gap: 1rem;
+    .socialLinks{
+        display: flex;
+        gap: 1rem;
+        color: whitesmoke;
+        li{
+            text-align: center;
+            line-height: 50px;
+            width: 50px;
+            height: 50px;
+            border-radius: 5px;
+            background-color: #007166;
+        }
+    }
+}
+
+</style>
