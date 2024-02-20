4771112500c83a8585a2bca9ede85fb62eb6b367
NicolaTeora <nikxodia@gmail.com>
2024-02-20 15:56:22 +0100

    feat : end shopProductList

--- src/components/ShopProductList.vue
+++ src/components/ShopProductList.vue
@@ -6,16 +6,19 @@ export default {
                 {
                     pic: "./assets/img/s1.jpg",
                     name: "Organic Juice",
+                    discount: 'SALE!',
                     price: 20,
                 },
                 {
                     pic: "./assets/img/s2.jpg",
                     name: "Fress Cobies",
+                    discount: 'SALE!',
                     price: 18,
                 },
                 {
                     pic: "./assets/img/s4.jpg",
                     name: "Fresh Blueberries",
+                    discount: 'SALE!',
                     price: 19,
                 },
                 {
@@ -26,24 +29,29 @@ export default {
                 {
                     pic: "./assets/img/s7.jpg",
                     name: "Naga pepper",
+                    discount: '',
                     price: 21,
                 },
                 {
                     pic: "./assets/img/s8.jpg",
                     name: "Fresh Waterlemon",
+                    discount: '',
                     price: 15,
                 },
                 {
                     pic: "./assets/img/s9.jpg",
                     name: "Red Gajor",
+                    discount: 'SALE!',
                     price: 29,
                 },
                 {
                     pic: "./assets/img/s10.jpg",
                     name: "organic letus",
+                    discount: '',
                     price: 23,
                 },
-            ]
+            ],
+            //shopMenus:[]
         }
     },
     methods:{
@@ -57,22 +65,27 @@ export default {
 <template>
     <div class="container-one">
         <div class="container-two">
-            <p>Trending Online Store</p>
-            <h2>Gogrin all <span>organic</span> food</h2>
-            
-            <p>Trending Online Store</p>
-            <h2>Gogrin all <span>organic</span> food</h2>
-            <ul>
-                <li>All product</li>
-                <li>Apple</li>
-                <li>food</li>
-                <li>Orange</li>
-                <li>Vegetable</li>
-            </ul>
+            <div class="menu-shop ">
+                <p>Trending Online Store</p>
+                <h2>Gogrin all <span>organic</span> food</h2>
+                
+                <p>Trending Online Store</p>
+                <h2>Gogrin all <span>organic</span> food</h2>
+                <ul>
+                    <li>All product</li>
+                    <li>Apple</li>
+                    <li>food</li>
+                    <li>Orange</li>
+                    <li>Vegetable</li>
+                </ul>
+            </div>
+
             <div class="products">
+                <!-- TODO: mettere il prezzo scontato -->
                 <div class="product" v-for="product of products">
                     <figure>
                         <img :src="getImgPath(product.pic)" :alt="product.pic">
+                        <span class="label">{{ product.discount }}</span>
                         <figcaption>
                             <p>{{ product.name }}</p>
                             <span>$ {{ product.price }}</span>
@@ -98,6 +111,19 @@ export default {
         padding: 2rem 0;
         text-align: center;
         color: white;
+        .menu-shop{
+            width: 50%;
+            margin: 20px auto;
+            p{
+                color: #EF9E03;
+                margin-top: 1rem;
+            }
+            h2{
+                margin-bottom: 1.5rem;
+                font-size: 55px;
+                span{ color: #EF9E03; }
+            }
+        }
         ul{
             display: flex;
             justify-content: space-around;
@@ -111,6 +137,26 @@ export default {
             flex-wrap: wrap;
             .product{
                 width: 25%;
+            }
+            p{
+                font-size: large;
+                margin: 10px 0;
+            }
+            span{
+                color: #EF9E03;
+                //margin-bottom: 10px;
+            }
+            figure{
+                margin: 20px 0;
+                position: relative;
+                .label{
+                    position: absolute;
+                    padding: 5px;
+                    right: 25px;
+                    top: 5px;
+                    background-color: #EF9E03;
+                    color: whitesmoke;
+                } 
             }
         }
     }
